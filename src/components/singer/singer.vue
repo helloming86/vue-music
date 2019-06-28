af15294e463190982a120218371daefcfcc733e6
Nicky <helloming86@qq.com>
2019-06-28 23:36:00 +0800

    ObtainSingerData

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -3,11 +3,36 @@
 </template>
 
 <script>
+import { getSingerList } from 'api/singer'
+import { ERR_OK } from 'api/config'
+
 export default {
-  name: 'singer'
+  name: 'singer',
+  data () {
+    return {
+      singers: []
+    }
+  },
+  created () {
+    this._getSingerList()
+  },
+  methods: {
+    _getSingerList () {
+      getSingerList().then((res) => {
+        if (res.code === ERR_OK) {
+          this.singers = res.data.list
+          // console.log(this.singers)ObtainSingerData
+        }
+      })
+    }
+  }
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 8
+    width: 100%
 </style>
